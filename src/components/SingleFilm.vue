93694c1b8f8eaaafb54ab6a0a71a33192a5628ed
gianlucanied <gianluca.nieddu96@gmail.com>
2024-01-11 11:49:05 +0100

    Try: change language with img flag

--- src/components/SingleFilm.vue
+++ src/components/SingleFilm.vue
@@ -3,17 +3,33 @@ export default {
     name: 'SingleFilm',
     props: {
         info: Object
+    },
+    data() {
+        return {
+            languageFlags: {
+                'en': '',
+                'it': '',
+            }
+        };
+    },
+    methods: {
+        getLanguageFlag(language) {
+            return this.languageFlags[language] || '';
+        }
     }
 }
 </script>
 
 <template>
-<h4>{{ info.original_title }}</h4>
-<h4>{{ info.title }}</h4>
-<h4>{{ info.original_language }}</h4>
-<h4>{{ info.vote_average }}</h4>
-
+    <div>
+        <img :src="getLanguageFlag(info.original_language)" alt="">
+        <h4>{{ info.original_title }}</h4>
+        <h4>{{ info.title }}</h4>
+        <h4>{{ info.original_language }}</h4>
+        <h4>{{ info.vote_average }}</h4>
+    </div>
 </template>
+
 
 <style lang="scss" scoped>
 
